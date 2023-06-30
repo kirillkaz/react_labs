6dc73b76a37591fdf4c2c01009efc5d3d9de7151
kirill <kirillkazazaev@mail.ru>
2023-07-01 02:52:38 +1000

    v2

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -25,6 +25,16 @@ const listProf= [
   Умещают на маленьких экранах максимальный функционал.`}
 ]
 
+const listLearm = [
+  {prof: 'Веб-разработчик',
+   discr: 'Создает сложные и очень сложные сайты, продумывает, чтобы пользователям было быстро и удобно' },
+   {prof: 'Гейм-девелопер',
+   discr: 'Создает видеоигры для гостиных и карманов, погружает всех нас в новые миры' },
+   {prof: 'AI/ML-специалист',
+   discr: 'Использует в деле искусственный интеллект и машинное обучение, фактами и прогнозами делает бизнесу хорошо' }
+
+]
+
 const listImg = ["logo_dvfu.png", "logo_imct.png"]
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -50,8 +60,24 @@ function Tagline() {
 }
 
 function Button(props) {
+  const [value, setValue] = React.useState(props.val);
+  const [list, setList] = React.useState(props.lists[0]);
+
+  const click = () =>{
+    if (value == "Хочу учиться!") {
+      setValue("Хочу работать!")
+      setList(props.lists[1])
+    }
+    else {
+      setValue("Хочу учиться!")
+      setList(props.lists[0])
+    }
+  }
   return (
-    <input className="button" type="button" value={props.val} />
+    <>
+    <input className="button" type="button" value={value} onClick={click} />
+    <Professions title="Обучаем на:" list={list} />
+    </>
   )
 }
 
@@ -96,8 +122,7 @@ function Content() {
     <>
       <Head listImg={listImg} />
       <Tagline/>
-      <Button val="Хочу учиться!"/>
-      <Professions title="Обучаем на:" list={listProf} />
+      <Button val="Хочу учиться!" lists={[listProf, listLearm]}/>
     </>
   )
 }
